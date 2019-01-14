9aefbebe2b8b55131516ae97f53f5486de8c500c
Tom George <tg82490@gmail.com>
2019-01-14 07:44:50 -0600

    Don't explicitly set a report path and see where it goes

--- pipeline.groovy
+++ pipeline.groovy
@@ -42,7 +42,7 @@ pipeline {
               --detect.project.name="RHLMDEMO-${ARTIFACT_NAME}" \
               --detect.policy.check.fail.on.severities=BLOCKER,CRITICAL \
               --detect.risk.report.pdf=true \
-              --detect.risk.report.pdf.path="./scanreports/" \
+              // --detect.risk.report.pdf.path="./scanreports/" \
               --blackduck.hub.trust.cert=true'
           }
         sh "ls -lrt"
