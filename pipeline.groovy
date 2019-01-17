6ffd3a6cceda40178b288ca9a0bd50fcd4a38017
Tom George <tg82490@gmail.com>
2019-01-17 11:53:15 -0600

    Removed CONTEXT_DIR, do some cleanup

--- pipeline.groovy
+++ pipeline.groovy
@@ -21,6 +21,7 @@ pipeline {
           env.HUB_URL = 'https://redhathub.blackducksoftware.com'
           env.NEXUS_USERNAME = 'admin'
           env.NEXUS_PASSWORD='admin123'
+          env.NEXUS_DESTINATION_REPOSITORY = 'lm-approved'
         }
       }
     }
@@ -46,7 +47,6 @@ pipeline {
               --detect.risk.report.pdf.path="./scanreports/" \
               --blackduck.hub.trust.cert=true'
           }
-        sh "ls -lrt"
         archiveArtifacts(artifacts: '**/scanreports/**')
         sh 'find . -name "*RiskReport.pdf" > ./repfilepath'
       }
@@ -64,7 +64,6 @@ pipeline {
               curl -H "X-Auth-Token: ${RC_TOKEN}" -H "X-User-Id: ${RC_USERNAME}" -H "Content-type:application/json" ${RC_URL}/api/v1/chat.postMessage -d '{ "channel": "#foss-compliance-pipeline", "text": "${message}" }'
                 """
           }
-          input( message: "Approve ${ARTIFACT_NAME}?")
         }
       }
    }
@@ -85,43 +84,29 @@ pipeline {
    {
       steps {
         script {
-          sh 'pwd; ls -lrt'
           def reportPath = readFile('./repfilepath').trim()
-          print "rep:" + reportPath
-          sh 'cat ./repfilepath'
-
-          dir ("${CONTEXT_DIR}")
-          {
-            def nexusurl = "${NEXUS_URL}/repository/lm-approved/"
-            def todaysdate = new Date()
-            uploadPath = todaysdate.format("YYYY/MM/dd/HH-mm-ss");
-            print uploadPath
-            reportPath = readFile('repfilepath').trim()
-            print "rep:" + reportPath
-            sh "curl -k -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} -X PUT " + nexusurl + uploadPath + "/scan-report.pdf" + " -T " + reportPath
-        
-            sh "find /home/jenkins/.m2/repository -name '*${ARTIFACT_NAME}*' > uploadfiles"
-            packagePath = readFile('uploadfiles').trim()
-            sh "zip -r ${ARTIFACT_NAME}.zip ."
-            sh "curl -k -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} -X PUT " + nexusurl + uploadPath + "/${ARTIFACT_NAME}.zip" + " -T ${ARTIFACT_NAME}.zip" 
-
-            env.NEXUS_ARTIFACT_URL = nexusurl + uploadPath 
-          }
+          def uploadUrl = "${NEXUS_URL}/repository/${env.NEXUS_DESTINATION_REPOSITORY}/"
+          def uploadPath = new Date().format("YYYY/MM/dd/HH-mm-ss");
+          sh "curl -k -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} -X PUT " + uploadUrl + uploadPath + "/scan-report.pdf" + " -T " + reportPath
+          sh "find /home/jenkins/.m2/repository -name '*${ARTIFACT_NAME}*' > uploadfiles"
+          packagePath = readFile('uploadfiles').trim()
+          sh "zip -r ${ARTIFACT_NAME}.zip ."
+          sh "curl -k -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} -X PUT " + uploadUrl + uploadPath + "/${ARTIFACT_NAME}.zip" + " -T ${ARTIFACT_NAME}.zip" 
+          env.NEXUS_ARTIFACT_URL = uploadUrl + uploadPath 
+          print "artifact URL ${env.NEXUS_ARTIFACT_URL}"
         }
       }
    }
 
    stage('Notify Users') {
-      steps {
-        script {
-          dir ("${CONTEXT_DIR}") {
-            def message = "The following artifacts have been approved: ${ARTIFACT_NAME}. They can be accessed at ${NEXUS_URL}"
-           sh """
-            curl -H "X-Auth-Token: ${ROCKET_CHAT_TOKEN_CREDENTIALS_ID}" -H "X-User-Id: ${ROCKET_CHAT_USERNAME_CREDENTIALS_ID}" -H "Content-type:application/json" ${RC_URL}/api/v1/chat.postMessage -d '{ "channel": "#foss-compliance-pipeline", "text": "${message}" }'
-              """
-          }
-        }
-      }
+     steps {
+       script {
+         def message = "The following artifacts have been approved: ${ARTIFACT_NAME}. They can be accessed at ${NEXUS_URL}"
+         sh """
+           curl -H "X-Auth-Token: ${ROCKET_CHAT_TOKEN_CREDENTIALS_ID}" -H "X-User-Id: ${ROCKET_CHAT_USERNAME_CREDENTIALS_ID}" -H "Content-type:application/json" ${RC_URL}/api/v1/chat.postMessage -d '{ "channel": "#foss-compliance-pipeline", "text": "${message}" }'
+         """
+       }
+     }
    }
  }
 }
