7eb3f8b91df4b99689c321ac773976778a82b422
Tom George <tg82490@gmail.com>
2019-01-26 12:14:46 -0600

    Do I need to look anywhere special to find the jar file

--- pipeline.groovy
+++ pipeline.groovy
@@ -14,7 +14,7 @@ pipeline {
         script {
           // env.NEXUS_URL = 'http://nexus-memcached-operator.apps.d2.casl.rht-labs.com/'
           // env.RC_URL = 'https://chat.consulting.redhat.com'
-          env.HUB_URL = 'https://redhathub.blackducksoftware.com'
+          env.HUB_URL = params.BLACKDUCK_URL 
           // env.NEXUS_USERNAME = 'admin'
           // env.NEXUS_PASSWORD='admin123'
           env.NEXUS_DESTINATION_REPOSITORY = 'lm-approved'
@@ -72,7 +72,7 @@ pipeline {
             def uploadUrl = "${NEXUS_URL}/repository/${env.NEXUS_DESTINATION_REPOSITORY}/"
             def uploadPath = new Date().format("YYYY/MM/dd/HH-mm-ss");
             sh "curl -k -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} -X PUT " + uploadUrl + uploadPath + "/scan-report.pdf" + " -T " + reportPath
-            sh "find /home/jenkins/.m2/repository -name '*${ARTIFACT_NAME}*' > uploadfiles"
+            sh "find . -name '*${ARTIFACT_NAME}*' > uploadfiles"
             packagePath = readFile('uploadfiles').trim()
             sh "zip -r ${ARTIFACT_NAME}.zip ."
             sh "curl -k -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} -X PUT " + uploadUrl + uploadPath + "/${ARTIFACT_NAME}.zip" + " -T ${ARTIFACT_NAME}.zip" 
